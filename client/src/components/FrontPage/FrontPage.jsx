7e9b4e608abfcc15b4b4550717905732284cbadc
manu-andaluz <manupeandaluz@gmail.com>
2022-11-29 21:43:18 -0300

    changing the front page styles and adding icons to the text

--- client/src/components/FrontPage/FrontPage.jsx
+++ client/src/components/FrontPage/FrontPage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./FrontPage.css";
+import { Link } from "react-router-dom";
 
 export default function FrontPage() {
   return (
     <div className="background-image">
       <div className="front-cover">
-        <h1>We believe that every dog deserves a forever loving home</h1>
+        <h1>We believe that every dog deserves a forever loving home ❤️</h1>
+        <Link to="/home">
+          <button className="btn-home">HOME 🏡</button>
+        </Link>
       </div>
     </div>
   );
