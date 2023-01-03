6b72317cb3bf6a00b2836ad07a207eda059735ec
manu-andaluz <manupeandaluz@gmail.com>
2023-01-03 16:10:36 -0300

    adding new navbar component and styles

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,73 +2,29 @@ import React from "react";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
+import { useRef } from "react";
 
 export default function NavBar() {
-  let activeStyle = {
-    borderBottom: "1px solid #3a3aac",
-    paddingBottom: "5px",
-    color: "#3a3aac",
-  };
+  const navRef = useRef();
 
-  const [toggle, setToggle] = useState({
-    burger: "burger",
-    containerLinks: "container-links",
-  });
-
-  const hanldeClick = () => {
-    if (toggle.containerLinks === "container-links") {
-      setToggle({
-        ...toggle,
-        burger: "burger toggle",
-        containerLinks: "nav-active",
-      });
-    } else {
-      setToggle({
-        ...toggle,
-        burger: "burger",
-        containerLinks: "container-links",
-      });
-    }
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
   };
 
   return (
-    <nav className="nav-bar">
-      <div className="logo">
-        <a href="/">Breed Dogs</a>
-      </div>
-
-      <ul className={toggle.containerLinks}>
-        <li className="nav-links">
-          <NavLink
-            to="/home"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            Home
-          </NavLink>
-        </li>
-        <li className="nav-links">
-          <NavLink
-            to="/createBread"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            Create Breed
-          </NavLink>
-        </li>
-        <li className="nav-links">
-          <NavLink
-            to="/aboutUs"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            About Us
-          </NavLink>
-        </li>
-      </ul>
-
-      <div className={toggle.burger} id="burger" onClick={hanldeClick}>
-        <div className="line1"></div>
-        <div className="line2"></div>
-        <div className="line3"></div>
-      </div>
-    </nav>
+    <header>
+      <h3>LOGO</h3>
+      <nav ref={navRef}>
+        <NavLink to="/home">Home</NavLink>
+        <NavLink to="/createBread">Create Breed</NavLink>
+        <NavLink to="/aboutUs">About Us</NavLink>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <i class="fa-solid fa-x"></i>
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <i class="fa-solid fa-bars"></i>
+      </button>
+    </header>
   );
 }
